52474f61749b408728f8f572f087bd50b2060dd4
Rezky Nur Fauzi <55779378+rezkynurfauzi@users.noreply.github.com>
2019-09-26 12:42:32 +0700

    Add files via upload

---
+++ IF01-10118016-Latihan10-Logika/src/logika/Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logika;
+
+/**
+ *
+ * Nama : Rezky Nur Fauzi
+ * NIM  : 10118016  
+ * Kelas: IF-1 
+ * Deskripsi Program : penggunaan boolean pada Java
+ */
+public class Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
